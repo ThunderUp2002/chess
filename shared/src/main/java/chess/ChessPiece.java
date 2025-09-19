10469da98041b7a6332a59dae233e33bf2351279
Devan Gwynn <devangwynn@gmail.com>
2025-09-19 01:35:19 -0600

    Override equals() and hashCode() methods for ChessBoard, ChessMove, ChessPiece, and ChessPosition

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -17,6 +18,20 @@ public class ChessPiece {
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -54,9 +69,34 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
-        if (piece.getPieceType() == PieceType.BISHOP) {
+
+        if (piece.getPieceType() == PieceType.KING) {
+            return List.of();
+        }
+
+        else if (piece.getPieceType() == PieceType.QUEEN) {
+            return List.of();
+        }
+
+        else if (piece.getPieceType() == PieceType.BISHOP) {
             return List.of(new ChessMove(new ChessPosition (5, 4), new ChessPosition(1, 8), null));
         }
-        return List.of();
+
+        else if (piece.getPieceType() == PieceType.KNIGHT) {
+            return List.of();
+        }
+
+        else if (piece.getPieceType() == PieceType.ROOK) {
+            return List.of();
+        }
+
+        else if (piece.getPieceType() == PieceType.PAWN) {
+            return List.of();
+        }
+
+        else {
+            // Return error?
+            return List.of();
+        }
     }
 }
