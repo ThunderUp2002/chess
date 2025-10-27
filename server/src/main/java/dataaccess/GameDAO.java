1ae236b773e0cff9fe7c4035f0c1e25cecd6ab1b
Devan Gwynn <devangwynn@gmail.com>
2025-10-27 12:46:04 -0600

    Create DAO interfaces

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,4 +1,16 @@
 package dataaccess;
 
+import model.*;
+import chess.*;
+
 public interface GameDAO {
+    GameData createGame(GameData gameData) throws DataAccessException;
+
+    GameData getGame(int gameID) throws DataAccessException;
+
+    ChessGame updateGame(String playerColor, int gameID, String username) throws DataAccessException;
+
+    void deleteGames() throws DataAccessException;
+
+    // GameList listGames() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,11 @@
 package dataaccess;
 
+import model.*;
+
 public interface UserDAO {
+    UserData createUser(UserData userData) throws DataAccessException;
+
+    UserData getUser(String username) throws DataAccessException;
+
+    void deleteUsers(String username) throws DataAccessException;
 }
