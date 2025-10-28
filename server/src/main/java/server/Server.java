2a1d71c99f637b69d1e0757ead67bd98e4ed5882
Devan Gwynn <devangwynn@gmail.com>
2025-10-28 17:06:09 -0600

    Register endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,21 @@
 package server;
 
 import io.javalin.*;
+import io.javalin.http.Context;
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
-
-        // Register your endpoints and exception handlers here.
-
+        javalin = Javalin.create(config -> config.staticFiles.add("web"))
+                .post("/user", this::register)
+                .post("/session", this::login)
+                .delete("/session", this::logout)
+                .get("/game", this::listGames)
+                .post("/game", this::createGame)
+                .put("/game", this::joinGame)
+                .delete("/db", this::clear);
     }
 
     public int run(int desiredPort) {
@@ -20,5 +25,33 @@ public class Server {
 
     public void stop() {
         javalin.stop();
+    }
+
+    private void register(Context cxt) {
+
+    }
+
+    private void login(Context cxt) {
+
+    }
+
+    private void logout(Context cxt) {
+
+    }
+
+    private void listGames(Context cxt) {
+
+    }
+
+    private void createGame(Context cxt) {
+
+    }
+
+    private void joinGame(Context cxt) {
+
+    }
+
+    private void clear(Context cxt) {
+
     }
 }
