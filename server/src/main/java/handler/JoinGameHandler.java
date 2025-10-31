63fe45bcfc49e2b839cd34bb87976fe398eb52a7
Devan Gwynn <devangwynn@gmail.com>
2025-10-31 16:18:23 -0600

    Implement JoinGameHandler

--- server/src/main/java/handler/JoinGameHandler.java
+++ server/src/main/java/handler/JoinGameHandler.java
@@ -1,4 +1,34 @@
 package handler;
 
+import com.google.gson.Gson;
+import exceptions.AlreadyTakenException;
+import exceptions.BadRequestException;
+import exceptions.UnauthorizedException;
+import io.javalin.http.Context;
+import requests.JoinGameRequest;
+import responses.ErrorResponse;
+import service.GameService;
+
 public class JoinGameHandler {
+
+    public static void handle(Context cxt, GameService gameService) throws Exception {
+        try {
+            String authToken = cxt.header("authorization");
+            if (authToken == null || authToken.isEmpty()) {
+                throw new UnauthorizedException("Error: unauthorized");
+            }
+            Gson gson = new Gson();
+            JoinGameRequest request = gson.fromJson(cxt.body(), JoinGameRequest.class);
+            gameService.joinGame(request, authToken);
+            cxt.status(200).result("{}");
+        } catch (BadRequestException e) {
+            cxt.status(400).result(new Gson().toJson(new ErrorResponse("Error: bad request")));
+        } catch (UnauthorizedException e) {
+            cxt.status(401).result(new Gson().toJson(new ErrorResponse("Error: unauthorized")));
+        } catch (AlreadyTakenException e) {
+            cxt.status(403).result(new Gson().toJson(new ErrorResponse("Error: already taken")));
+        } catch (Exception e) {
+            cxt.status(500).result(new Gson().toJson(new ErrorResponse("Error: something went wrong")));
+        }
+    }
 }
