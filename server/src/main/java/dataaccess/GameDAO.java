0c1368777a94399ce69574ff968bc3210d9c07b8
Devan Gwynn <devangwynn@gmail.com>
2025-10-31 00:20:01 -0600

    Change createGame() and updateGame() to return void

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,15 +1,14 @@
 package dataaccess;
 
 import model.*;
-import chess.*;
 import java.util.Collection;
 
 public interface GameDAO {
-    GameData createGame(GameData gameData) throws DataAccessException;
+    void createGame(GameData gameData) throws DataAccessException;
 
     GameData getGame(int gameID) throws DataAccessException;
 
-    ChessGame updateGame(String playerColor, int gameID, String username) throws DataAccessException;
+    void updateGame(String playerColor, int gameID, String username) throws DataAccessException;
 
     Collection<GameData> listGames() throws DataAccessException;
 
