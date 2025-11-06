03d476f05a10a66d4e2d429787800f858785a134
Devan Gwynn <devangwynn@gmail.com>
2025-11-06 12:32:54 -0700

    Switch to using database instead of memory, and remove unnecessary exceptions on endpoint methods

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,13 +10,9 @@ public class Server {
 
     private final Javalin javalin;
 
-    private final AuthDAO authDAO = new MemoryAuthDAO();
-    private final GameDAO gameDAO = new MemoryGameDAO();
-    private final UserDAO userDAO = new MemoryUserDAO();
-
-    private final ClearService clearService = new ClearService(authDAO, gameDAO, userDAO);
-    private final GameService gameService = new GameService(authDAO, gameDAO);
-    private final UserService userService = new UserService(authDAO, userDAO);
+    private ClearService clearService;
+    private GameService gameService;
+    private UserService userService;
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
@@ -30,39 +26,52 @@ public class Server {
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        try {
+            AuthDAO authDAO = new DatabaseAuthDAO();
+            GameDAO gameDAO = new DatabaseGameDAO();
+            UserDAO userDAO = new DatabaseUserDAO();
+
+            this.clearService = new ClearService(authDAO, gameDAO, userDAO);
+            this.gameService = new GameService(authDAO, gameDAO);
+            this.userService = new UserService(authDAO, userDAO);
+
+            javalin.start(desiredPort);
+            return javalin.port();
+        } catch (DataAccessException e) {
+            stop();
+            return -1;
+        }
     }
 
     public void stop() {
         javalin.stop();
     }
 
-    private void register(Context cxt) throws Exception {
+    private void register(Context cxt) {
         RegisterHandler.handle(cxt, userService);
     }
 
-    private void login(Context cxt) throws Exception {
+    private void login(Context cxt) {
         LoginHandler.handle(cxt, userService);
     }
 
-    private void logout(Context cxt) throws Exception {
+    private void logout(Context cxt) {
         LogoutHandler.handle(cxt, userService);
     }
 
-    private void listGames(Context cxt) throws Exception {
+    private void listGames(Context cxt) {
         ListGamesHandler.handle(cxt, gameService);
     }
 
-    private void createGame(Context cxt) throws Exception {
+    private void createGame(Context cxt) {
         CreateGameHandler.handle(cxt, gameService);
     }
 
-    private void joinGame(Context cxt) throws Exception {
+    private void joinGame(Context cxt) {
         JoinGameHandler.handle(cxt, gameService);
     }
 
-    private void clear(Context cxt) throws DataAccessException {
+    private void clear(Context cxt) {
         ClearHandler.handle(cxt, clearService);
     }
 }
