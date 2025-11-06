67b75efcd1857a78115e48bf2d5d75ddbe84b214
Devan Gwynn <devangwynn@gmail.com>
2025-11-06 12:28:34 -0700

    Implement DatabaseGameDAO

--- server/src/main/java/dataaccess/DatabaseGameDAO.java
+++ server/src/main/java/dataaccess/DatabaseGameDAO.java
@@ -1,33 +1,133 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class DatabaseGameDAO implements GameDAO {
+    private static final String createStatement =
+            """
+            CREATE TABLE IF NOT EXISTS games (
+            gameID INT PRIMARY KEY NOT NULL UNIQUE,
+            whiteUsername VARCHAR(256),
+            blackUsername VARCHAR(256),
+            gameName VARCHAR(256) NOT NULL,
+            chessGame LONGTEXT NOT NULL
+            )
+            """;
+
+    public DatabaseGameDAO() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(createStatement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: database configuration failed");
+        }
+    }
+
     @Override
     public void createGame(GameData gameData) throws DataAccessException {
-
+        var statement = "INSERT INTO games (gameID, whiteUsername, blackUsername, gameName, chessGame) VALUES (?, ?, ?, ?, ?)";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setInt(1, gameData.gameID());
+                preparedStatement.setString(2, gameData.whiteUsername());
+                preparedStatement.setString(3, gameData.blackUsername());
+                preparedStatement.setString(4, gameData.gameName());
+                var json = new Gson().toJson(gameData.game());
+                preparedStatement.setString(5, json);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: database error");
+        }
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, chessGame FROM games WHERE gameID=?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setInt(1, gameID);
+                try (var resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        var json = resultSet.getString("chessGame");
+                        var chessGame = new Gson().fromJson(json, ChessGame.class);
+                        return new GameData(resultSet.getInt("gameID"), resultSet.getString("whiteUsername"), resultSet.getString("blackUsername"), resultSet.getString("gameName"), chessGame);
+                    }
+                    else {
+                        throw new DataAccessException("Error: database error");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: database error");
+        }
     }
 
     @Override
     public void updateGame(String playerColor, int gameID, String username) throws DataAccessException {
-
+        GameData gameData = getGame(gameID);
+        var statement = "DELETE FROM games WHERE gameID=?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.setInt(1, gameID);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: database error");
+        }
+        ChessGame chessGame = new ChessGame();
+        GameData newGameData;
+        if (playerColor.equalsIgnoreCase("white")) {
+            newGameData = new GameData(gameID, username, gameData.blackUsername(), gameData.gameName(), chessGame);
+        }
+        else {
+            newGameData = new GameData(gameID, gameData.whiteUsername(), username, gameData.gameName(), chessGame);
+        }
+        createGame(newGameData);
     }
 
     @Override
     public Collection<GameData> listGames() throws DataAccessException {
-        return List.of();
+        var games = new ArrayList<GameData>();
+        var statement = "SELECT gameID, whiteUsername, blackUsername, gameName, chessGame FROM games";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                try (var resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        var gameID = resultSet.getInt("gameID");
+                        var whiteUsername = resultSet.getString("whiteUsername");
+                        var blackUsername = resultSet.getString("blackUsername");
+                        var gameName = resultSet.getString("gameName");
+                        var json = resultSet.getString("chessGame");
+                        var chessGame = new Gson().fromJson(json, ChessGame.class);
+
+                        games.add(new GameData(gameID, whiteUsername, blackUsername, gameName, chessGame));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: database error");
+        }
+        return games;
     }
 
     @Override
     public void deleteGames() throws DataAccessException {
-
+        var statement = "TRUNCATE games";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Error: database error");
+        }
     }
 }
