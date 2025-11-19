0b45c6d9f6cf2581b6966fb58b097670cfa03a75
Devan Gwynn <devangwynn@gmail.com>
2025-11-19 00:04:23 -0700

    Modify to run ChessClient

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,8 @@
-import chess.*;
+import client.ChessClient;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        var serverUrl = "http://localhost:8080";
+        new ChessClient(serverUrl).run();
     }
 }
