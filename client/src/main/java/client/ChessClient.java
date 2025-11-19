5ee2798f6b1b5eeba24bf9eff8e4fccd827472db
Devan Gwynn <devangwynn@gmail.com>
2025-11-19 00:48:00 -0700

    Implement ChessClient

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,4 +1,92 @@
 package client;
 
+import responses.LoginResponse;
+import responses.RegisterResponse;
+import server.ServerFacade;
+import ui.PostLoginUI;
+import ui.PreLoginUI;
+
+import java.util.Scanner;
+
+import static ui.PreLoginUI.*;
+import static ui.PostLoginUI.*;
+
 public class ChessClient {
+    private final ServerFacade facade;
+    private String authToken;
+    private State state = State.LOGGED_OUT;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public enum State {
+        LOGGED_IN,
+        LOGGED_OUT
+    }
+
+    public ChessClient(String serverUrl) {
+        facade = new ServerFacade(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("Welcome to the chess application. Type 'help' to begin.");
+        while (true) {
+            try {
+                if (state == State.LOGGED_OUT) {
+                    System.out.print("[LOGGED OUT] >>> ");
+                    String input = scanner.nextLine();
+                    handlePreLogin(input);
+                } else {
+                    System.out.print("[LOGGED IN] >>> ");
+                    String input = scanner.nextLine();
+                    handlePostLogin(input);
+                }
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+        }
+    }
+
+    private void handlePreLogin(String command) {
+        switch(command.toLowerCase()) {
+            case "help" -> PreLoginUI.help();
+            case "quit" -> quit();
+            case "login" -> {
+                LoginResponse response = login(facade);
+                if (response != null) {
+                    authToken = response.authToken();
+                    state = State.LOGGED_IN;
+                    System.out.println("Logged in successfully");
+                } else {
+                    System.out.println("Login failed");
+                }
+            }
+            case "register" -> {
+                RegisterResponse response = register(facade);
+                if (response != null) {
+                    authToken = response.authToken();
+                    state = State.LOGGED_IN;
+                    System.out.println("Registered successfully");
+                } else {
+                    System.out.println("Registration failed");
+                }
+            }
+            default -> System.out.println("Unknown command. Type 'help' to see a list of available commands.");
+        }
+    }
+
+    private void handlePostLogin(String command) throws Exception {
+        switch(command.toLowerCase()) {
+            case "list" -> listGames(facade, authToken);
+            case "create" -> createGame(facade, authToken);
+            case "play" -> playGame(facade, authToken);
+            case "observe" -> observeGame(facade, authToken);
+            case "logout" -> {
+                logout(facade, authToken);
+                authToken = null;
+                state = State.LOGGED_OUT;
+                System.out.println("Logged out successfully");
+            }
+            case "help" -> PostLoginUI.help();
+            default -> System.out.println("Unknown command. Type 'help' to see a list of available commands.");
+        }
+    }
 }
