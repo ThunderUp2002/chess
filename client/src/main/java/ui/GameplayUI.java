e706f98528feba5be4594c689ca6f615836edbfe
Devan Gwynn <devangwynn@gmail.com>
2025-12-10 06:05:35 -0700

    Implement highlight() and move()

--- client/src/main/java/ui/GameplayUI.java
+++ client/src/main/java/ui/GameplayUI.java
@@ -1,17 +1,21 @@
 package ui;
 
 import chess.ChessGame;
+import chess.ChessMove;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import model.GameData;
 import websocket.NotificationHandler;
 import websocket.WebSocketConnection;
+import websocket.commands.MakeMove;
 import websocket.commands.UserGameCommand;
 import websocket.messages.Error;
 import websocket.messages.LoadGame;
 import websocket.messages.Notification;
 import websocket.messages.ServerMessage;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
 
 import static ui.EscapeSequences.*;
@@ -24,6 +28,8 @@ public class GameplayUI implements NotificationHandler {
     private final GameData gameData;
     private WebSocketConnection webSocketConnection;
     private final static Scanner scanner = new Scanner(System.in);
+    private Collection<ChessMove> highlightedMoves = new ArrayList<>();
+    private ChessPosition highlightedPosition = null;
 
     public GameplayUI(GameData gameData, boolean isWhitePlayer, String authToken, WebSocketConnection webSocketConnection) {
         this.game = gameData.game();
@@ -36,16 +42,15 @@ public class GameplayUI implements NotificationHandler {
     public void run() {
         try {
             System.out.print(ERASE_SCREEN);
-            System.out.println("Joining game...");
             sendCommand(UserGameCommand.CommandType.CONNECT);
+            Thread.sleep(1000);
             help();
 
             while (isPlaying) {
                 System.out.print(RESET_BG_COLOR);
                 System.out.print(RESET_TEXT_COLOR);
                 System.out.println();
-                System.out.print(SET_TEXT_ITALIC + "[GAMEPLAY] >>> ");
-                System.out.print(RESET_TEXT_ITALIC);
+                printPrompt();
                 String input = scanner.nextLine().trim().toLowerCase();
 
                 switch (input) {
@@ -89,11 +94,100 @@ public class GameplayUI implements NotificationHandler {
     }
 
     public void highlight() {
+        System.out.print("Enter the position of the piece for which you would like to highlight legal moves (for example, a1): ");
+        String input = scanner.nextLine().trim().toLowerCase();
+        if (isInvalidPosition(input)) {
+            System.out.println("Invalid position");
+            return;
+        }
 
+        ChessPosition position = constructChessPosition(input);
+        ChessPiece piece = gameData.game().getBoard().getPiece(position);
+        if (piece == null) {
+            System.out.println("No piece on " + input);
+            return;
+        }
+
+        highlightedMoves = gameData.game().validMoves(position);
+        highlightedPosition = position;
+
+        displayBoard();
+
+        highlightedMoves = new ArrayList<>();
+        highlightedPosition = null;
     }
 
     public void move() {
+        if (isWhitePlayer && gameData.game().getTeamTurn().equals(ChessGame.TeamColor.BLACK)) {
+            System.out.println("You cannot make a move when it is not your turn");
+            return;
+        }
+        if (!isWhitePlayer && gameData.game().getTeamTurn().equals(ChessGame.TeamColor.WHITE)) {
+            System.out.println("You cannot make a move when it is not your turn");
+        }
+        System.out.print("Enter the position of the piece you would like to move (for example, a1): ");
+        String startingPosition = scanner.nextLine().trim().toLowerCase();
+        if (isInvalidPosition(startingPosition)) {
+            System.out.println("Invalid position");
+            return;
+        }
+        ChessPosition startPos = constructChessPosition(startingPosition);
+        if (gameData.game().getBoard().getPiece(startPos) == null) {
+            System.out.println("There is no piece on " + startingPosition);
+            return;
+        }
+        if (isWhitePlayer && gameData.game().getBoard().getPiece(startPos).getTeamColor().equals(ChessGame.TeamColor.BLACK)) {
+            System.out.println("You may not move the other player's pieces");
+            return;
+        }
+        if (!isWhitePlayer && gameData.game().getBoard().getPiece(startPos).getTeamColor().equals(ChessGame.TeamColor.WHITE)) {
+            System.out.println("You may not move the other player's pieces");
+            return;
+        }
+        System.out.print("Enter the position you would like to move this piece (for example, a1): ");
+        String endingPosition = scanner.nextLine().trim().toLowerCase();
+        if (isInvalidPosition(endingPosition)) {
+            System.out.println("Invalid position");
+            return;
+        }
+        ChessPosition endPos = constructChessPosition(endingPosition);
+        ChessPiece.PieceType type = gameData.game().getBoard().getPiece(startPos).getPieceType();
+        ChessMove move;
+        if (isWhitePlayer && endPos.getRow() == 8 && type == ChessPiece.PieceType.PAWN) {
+            System.out.print("Choose a piece to promote your pawn to (BISHOP/KNIGHT/ROOK/QUEEN): ");
+            String selection = scanner.nextLine().trim().toLowerCase();
+            move = new ChessMove(startPos, endPos, selectPromotionPiece(selection));
+        }
+        else if (!isWhitePlayer && endPos.getRow() == 1 && type == ChessPiece.PieceType.PAWN) {
+            System.out.print("Choose a piece to promote your pawn to (BISHOP/KNIGHT/ROOK/QUEEN): ");
+            String selection = scanner.nextLine().trim().toLowerCase();
+            move = new ChessMove(startPos, endPos, selectPromotionPiece(selection));
+        }
+        else {
+            move = new ChessMove(startPos, endPos, null);
+        }
+        MakeMove makeMove = new MakeMove(authToken, gameData.gameID(), move);
+        webSocketConnection.sendCommand(makeMove);
+    }
 
+    private ChessPiece.PieceType selectPromotionPiece(String selection) {
+        return switch (selection) {
+            case "bishop" -> ChessPiece.PieceType.BISHOP;
+            case "knight" -> ChessPiece.PieceType.KNIGHT;
+            case "rook" -> ChessPiece.PieceType.ROOK;
+            case "queen" -> ChessPiece.PieceType.QUEEN;
+            default -> null;
+        };
+    }
+
+    private ChessPosition constructChessPosition(String position) {
+        int col = position.charAt(0) - 'a' + 1;
+        int row = position.charAt(1) - '0';
+        return new ChessPosition(row, col);
+    }
+
+    private boolean isInvalidPosition(String position) {
+        return !position.matches("^[a-h][1-8]$");
     }
 
     public void resign() {
@@ -117,6 +211,8 @@ public class GameplayUI implements NotificationHandler {
         drawChessBoard();
         System.out.print(SET_BG_COLOR_BLACK);
         System.out.print(SET_TEXT_COLOR_WHITE);
+        System.out.print(RESET_BG_COLOR);
+        System.out.print(RESET_TEXT_COLOR);
     }
 
     private void drawChessBoard() {
@@ -151,7 +247,15 @@ public class GameplayUI implements NotificationHandler {
             boolean isLightSquare = (rowNum + colNum) % 2 == 0;
 
             ChessPosition position = new ChessPosition(8 - rowNum, colNum + 1);
-            System.out.print(isLightSquare ? SET_BG_COLOR_WHITE : SET_BG_COLOR_BLACK);
+            if (isPositionHighlighted(position)) {
+                if (isLightSquare) {
+                    System.out.print(SET_BG_COLOR_GREEN);
+                } else {
+                    System.out.print(SET_BG_COLOR_DARK_GREEN);
+                }
+            } else {
+                System.out.print(isLightSquare ? SET_BG_COLOR_WHITE : SET_BG_COLOR_BLACK);
+            }
 
             ChessPiece piece = game.getBoard().getPiece(position);
             if (piece == null) {
@@ -168,6 +272,13 @@ public class GameplayUI implements NotificationHandler {
             System.out.print(RESET_BG_COLOR);
             System.out.print(RESET_TEXT_COLOR);
         }
+    }
+
+    private boolean isPositionHighlighted(ChessPosition position) {
+        if (highlightedPosition != null && highlightedPosition.equals(position)) {
+            return true;
+        }
+        return highlightedMoves.stream().anyMatch(move -> move.getEndPosition().equals(position));
     }
 
     private void drawHeader() {
@@ -200,6 +311,7 @@ public class GameplayUI implements NotificationHandler {
                 LoadGame loadGame = (LoadGame) serverMessage;
                 this.game = loadGame.getGame().game();
                 displayBoard();
+                printPrompt();
                 break;
             case NOTIFICATION:
                 Notification notification = (Notification) serverMessage;
@@ -223,5 +335,10 @@ public class GameplayUI implements NotificationHandler {
 
     public void setWebSocketConnection(WebSocketConnection webSocketConnection) {
         this.webSocketConnection = webSocketConnection;
+    }
+
+    private void printPrompt() {
+        System.out.print(SET_TEXT_ITALIC + "[GAMEPLAY] >>> ");
+        System.out.print(RESET_TEXT_ITALIC);
     }
 }
